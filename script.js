172804ba136b7c323bb138023d7be22aca7d2c57
toragp <guillaule.castant@gmail.com>
2024-10-10 13:11:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
 
-    const fileUrl = 'port clef.pptx';
+    const fileUrl = 'Secrets du Morpion.pptx';
 
     // Créer un lien temporaire pour télécharger le fichier
     const a = document.createElement('a');
     a.href = fileUrl;
-    a.download = 'port clef.pptx'; // Nom du fichier téléchargé
+    a.download = 'Secrets du Morpion.pptx'; // Nom du fichier téléchargé
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a); // Supprimer le lien après téléchargement
